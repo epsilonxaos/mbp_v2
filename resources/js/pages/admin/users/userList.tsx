6950eb89f1ffb4ba81dcbf60d50943bcc41b78f6
Epsilonxaos <angelotr12@hotmail.com>
2025-05-01 20:57:54 -0600

    feat: Agrega tabla de usuarios con paginación y ordenamiento

--- resources/js/pages/admin/users/userList.tsx
+++ resources/js/pages/admin/users/userList.tsx
@@ -1,7 +1,20 @@
+import { Button } from '@/components/ui/button';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react';
-
+import { Head, useForm, usePage } from '@inertiajs/react';
+import {
+    ColumnDef,
+    SortingState,
+    Table as TableType,
+    flexRender,
+    getCoreRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from '@tanstack/react-table';
+import { ArrowUpDown } from 'lucide-react';
+import { useState } from 'react';
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Usuarios',
@@ -9,32 +22,244 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+interface PageProps {
+    users: UserList;
+    [key: string]: unknown; // Add index signature to satisfy the type constraint
+}
+
+type User = {
+    id: string;
+    name: string;
+    created_at: string;
+    updated_at: string;
+    email: string;
+};
+
+type UserList = User[];
+
 export default function UserList() {
+    const { users } = usePage<PageProps>().props;
+
+    const { data, setData, delete: destroy, processing, reset, errors, clearErrors } = useForm<Required<{ id: string }>>({ id: '' });
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Administrar usuarios" />
-            {/* <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="flex h-full flex-col items-center justify-center text-center">
-                    <h1 className="text-3xl font-bold text-neutral-900 dark:text-neutral-100">Bienvenido al Panel de Administración</h1>
-                    <p className="mt-2 text-lg text-neutral-700 dark:text-neutral-300">
-                        Aquí puedes gestionar las configuraciones y datos de la aplicación.
-                    </p>
-                    <div className="mt-6 grid gap-4 sm:grid-cols-2">
-                        <div className="rounded-lg border border-neutral-200 bg-white p-4 shadow-sm dark:border-neutral-700 dark:bg-neutral-800">
-                            <Users className="mx-auto mb-6" size={30} />
-                            <h2 className="text-xl font-semibold text-neutral-900 dark:text-neutral-100">Usuarios</h2>
-                            <p className="mt-1 text-sm text-neutral-600 dark:text-neutral-400">Gestiona los usuarios registrados en la plataforma.</p>
-                        </div>
-                        <div className="rounded-lg border border-neutral-200 bg-white p-4 shadow-sm dark:border-neutral-700 dark:bg-neutral-800">
-                            <PanelsTopLeft className="mx-auto mb-6" size={30} />
-                            <h2 className="text-xl font-semibold text-neutral-900 dark:text-neutral-100">Configuración</h2>
-                            <p className="mt-1 text-sm text-neutral-600 dark:text-neutral-400">
-                                Ajusta las configuraciones generales de la aplicación.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+
+            {/* <div className="px-6">
+                <Table>
+                    <TableCaption>A list of your recent invoices.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[100px]">ID</TableHead>
+                            <TableHead>Nombre</TableHead>
+                            <TableHead>Email</TableHead>
+                            <TableHead>Fecha de creación</TableHead>
+                            <TableHead>Acciones</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {users.map((user) => (
+                            <TableRow key={user.id}>
+                                <TableCell className="font-medium">{user.id}</TableCell>
+                                <TableCell>{user.name}</TableCell>
+                                <TableCell>{user.email}</TableCell>
+                                <TableCell>
+                                    {new Date(user.created_at).toLocaleDateString('es-ES', { day: '2-digit', month: 'long', year: 'numeric' })}
+                                </TableCell>
+                                <TableCell className="flex gap-2">
+                                   
+                                    <Button className="h-auto px-2 py-1 text-xs" variant="default" disabled={processing} asChild>
+                                        <button title="Editar cuenta" type="button">
+                                            <UserRoundPen />
+                                        </button>
+                                    </Button>
+                                    <Button className="h-auto px-2 py-1 text-xs" variant="destructive" disabled={processing} asChild>
+                                        <button title="Eliminar cuenta" type="button">
+                                            <UserRoundX />
+                                        </button>
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TableCell colSpan={3}>Total</TableCell>
+                            <TableCell className="text-right">$2,500.00</TableCell>
+                        </TableRow>
+                    </TableFooter>
+                </Table>
             </div> */}
+
+            <div className="container mx-auto p-10">
+                <DataTable columns={columns} data={users} />
+            </div>
         </AppLayout>
+    );
+}
+
+const columns: ColumnDef<User>[] = [
+    {
+        accessorKey: 'id',
+        header: 'ID',
+    },
+    {
+        accessorKey: 'name',
+        header: 'Nombre',
+    },
+    {
+        accessorKey: 'email',
+        header: ({ column }) => {
+            return (
+                <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+                    Email
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            );
+        },
+    },
+    {
+        accessorKey: 'created_at',
+        header: 'Fecha de creación',
+    },
+    {
+        accessorKey: 'updated_at',
+        header: 'Última actualización',
+    },
+];
+
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+}
+
+function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+    const [sorting, setSorting] = useState<SortingState>([]);
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+        state: {
+            sorting,
+        },
+    });
+
+    return (
+        <section>
+            {/* Tablero de datos */}
+            <div className="rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                                        </TableHead>
+                                    );
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={columns.length} className="h-24 text-center">
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+
+            {/* Pagination */}
+            <DataTablePagination table={table} />
+        </section>
+    );
+}
+
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from 'lucide-react';
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+
+interface DataTablePaginationProps<TData> {
+    table: TableType<TData>;
+}
+
+function DataTablePagination<TData>({ table }: DataTablePaginationProps<TData>) {
+    return (
+        <div className="flex items-center justify-between px-2">
+            <div className="text-muted-foreground flex-1 text-sm">
+                {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected.
+            </div>
+            <div className="flex items-center space-x-6 lg:space-x-8">
+                <div className="flex items-center space-x-2">
+                    <p className="text-sm font-medium">Rows per page</p>
+                    <Select
+                        value={`${table.getState().pagination.pageSize}`}
+                        onValueChange={(value) => {
+                            table.setPageSize(Number(value));
+                        }}
+                    >
+                        <SelectTrigger className="h-8 w-[70px]">
+                            <SelectValue placeholder={table.getState().pagination.pageSize} />
+                        </SelectTrigger>
+                        <SelectContent side="top">
+                            {[10, 20, 30, 40, 50].map((pageSize) => (
+                                <SelectItem key={pageSize} value={`${pageSize}`}>
+                                    {pageSize}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
+                </div>
+                <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+                    Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+                </div>
+                <div className="flex items-center space-x-2">
+                    <Button
+                        variant="outline"
+                        className="hidden h-8 w-8 p-0 lg:flex"
+                        onClick={() => table.setPageIndex(0)}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        <span className="sr-only">Go to first page</span>
+                        <ChevronsLeft />
+                    </Button>
+                    <Button variant="outline" className="h-8 w-8 p-0" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+                        <span className="sr-only">Go to previous page</span>
+                        <ChevronLeft />
+                    </Button>
+                    <Button variant="outline" className="h-8 w-8 p-0" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+                        <span className="sr-only">Go to next page</span>
+                        <ChevronRight />
+                    </Button>
+                    <Button
+                        variant="outline"
+                        className="hidden h-8 w-8 p-0 lg:flex"
+                        onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        <span className="sr-only">Go to last page</span>
+                        <ChevronsRight />
+                    </Button>
+                </div>
+            </div>
+        </div>
     );
 }
